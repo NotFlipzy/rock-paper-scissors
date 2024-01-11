f28beeb121ce44e3f5b48e493737b9868382a87a
Evan Vega <evan.r.vega@outlook.com>
2024-01-10 19:06:09 -0500

    Fixed bug where entering an incorrect option would quit the game.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock-Paper-Scissors
+
+This is my first game in Python for my Computer Science Class. Hope you Enjoy it!
--- game.py
+++ game.py
@@ -1,10 +1,3 @@
-"""
-Title: Rock Paper Scissors Game
-Author: Evan Vega
-Description: This is my first game in Python for my Computer Science Class. Hope you Enjoy it!
-Date: 1/6/2024
-"""
-
 import random
 
 options = ["rock", "paper", "scissors"]
@@ -19,27 +12,29 @@ option = input("\nEnter one of the following options (Rock, Paper, Scissors, or 
 option = option.lower()
 
 while option != "quit":
-    if not option in options:
+    if option in options:      
+        random_option = random.choice(options)
+        print("You picked: " + option)
+        print("Computer picked: " + random_option)
+        if option == random_option:
+            print("Tie!!!")
+        elif option == "scissors" and random_option == "paper":
+            print("You Win!!!")
+            myscore = myscore + 1
+        elif option == "paper" and random_option == "rock":
+            print("You Win!!!")
+            myscore = myscore + 1
+        elif option == "rock" and random_option == "scissors":
+            print("You Win!!!")
+            myscore = myscore + 1
+        else:
+            print("Computer Wins!!!")
+            computerscore = computerscore + 1
+    else: 
         print("Incorrect Option")
-        exit()
-    random_option = random.choice(options)
-    print("You picked: " + option)
-    print("Computer picked: " + random_option)
-    if option == random_option:
-        print("Tie!!!")
-    elif option == "scissors" and random_option == "paper":
-        print("You Win!!!")
-        myscore = myscore + 1
-    elif option == "paper" and random_option == "rock":
-        print("You Win!!!")
-        myscore = myscore + 1
-    elif option == "rock" and random_option == "scissors":
-        print("You Win!!!")
-        myscore = myscore + 1
-    else:
-        print("Computer Wins!!!")
-        computerscore = computerscore + 1
 
     print("Your Score: "+ str(myscore), "Computer Score: " + str(computerscore))
     option = input("\nEnter one of the following options (Rock, Paper, Scissors, or Quit):")
     option = option.lower()
+
+print("Bye Bye!")
